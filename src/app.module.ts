0b68742d42edd59638ec6ce421e91a813e406f57
Umarbek <rustamtoshmamatov90@gmail.com>
2025-06-27 18:58:01 +0500

    app modul yaratildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CoursesModule } from './courses/courses.module';
+import { ModulesModule } from './modules/modules.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { AssignmentsModule } from './assignments/assignments.module';
+import { ResultsModule } from './results/results.module';
+import { EnrollmentsModule } from './enrollments/enrollments.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -12,11 +20,19 @@ import { ConfigModule } from '@nestjs/config';
       host: process.env.DB_HOST || 'localhost',
       port: +(process.env.DB_PORT ?? 5432),
       username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
+      password: process.env.DB_PASSWORD || '1234',
       database: process.env.DB_NAME || 'online_course',
       synchronize: true,
       autoLoadEntities: true,
     }),
+    UsersModule,
+    AuthModule,
+    CoursesModule,
+    ModulesModule,
+    LessonsModule,
+    AssignmentsModule,
+    ResultsModule,
+    EnrollmentsModule,
   ],
   controllers: [],
   providers: [],
