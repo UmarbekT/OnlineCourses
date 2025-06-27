29b2c1a3e826a13939695f46014a1b238464ff65
Umarbek <rustamtoshmamatov90@gmail.com>
2025-06-27 18:58:01 +0500

    main fayl yaratildi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:5000',
+    credentials: true,
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Online Course API')
+    .setDescription('Online kurslar tizimi uchun API hujjati')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
